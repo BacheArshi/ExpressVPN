7d9e834fbd0db7df637e4662ae8e2db28aee9d5e
expressalaki <189632912+expressalaki@users.noreply.github.com>
2026-02-23 18:26:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -71,13 +71,11 @@ def analyze_and_rename(config, channel_name, use_my_branding=False):
         if not clean_source.startswith("@"): clean_source = f"@{clean_source}"
 
         if use_my_branding:
-            final_label = f"{CUSTOM_SEPARATOR}"
-            # final_label = f"{MY_CHANNEL_ID} {CUSTOM_SEPARATOR} src {clean_source}" - به همراه منبع
-        
+            final_label = f"{MY_CHANNEL_ID} {CUSTOM_SEPARATOR} src {clean_source}"
             separator = CUSTOM_SEPARATOR
         else:
-            final_label = f"{MY_CHANNEL_ID} {CUSTOM_SEPARATOR}"
-            separator = CUSTOM_SEPARATOR
+            final_label = clean_source
+            separator = SOURCE_ICON
 
         transport, security, flag = "TCP", "None", NOT_FOUND_FLAG
         
@@ -225,7 +223,7 @@ def run():
     with open('data.temp', 'w', encoding='utf-8') as f:
         for item in valid_items: f.write("|".join(item) + "\n")
     
-    with open('pointer.txt', 'w', encoding='utf-8') as f:**
+    with open('pointer.txt', 'w', encoding='utf-8') as f:
         new_ptr = (current_index + ROTATION_LIMIT) % pool_size if pool_size > 0 else 0
         f.write(str(new_ptr))
 
